f35c6b6a7eaeeeef1379ecf6770bbbef7f58a26e
unknown <noemiferreras@alumnos.cei.es>
2023-12-22 18:54:11 +0100

    Version 1.4

--- script.js
+++ script.js
@@ -5,21 +5,37 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 /* Creamos el array de objetos*/
     const photos = [{
-        "img":"./img/gallery sushi (1).jpg"
+        "img":"./img/gallery sushi (1).jpg",
+        "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (2).jpg"
+        "img":"./img/gallery sushi (2).jpg",
+        "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (3).jpg"
+        "img":"./img/gallery sushi (3).jpg",
+        "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (4).jpg"
+        "img":"./img/gallery sushi (4).jpg",
+                "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (5).jpg"
+        "img":"./img/gallery sushi (5).jpg",
+                "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (6).jpg"
+        "img":"./img/gallery sushi (6).jpg",
+                "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (7).jpg"
+        "img":"./img/gallery sushi (7).jpg",
+                "texto": "Lorem ipsum",
+        "precio": 10.00,
     },{
-        "img":"./img/gallery sushi (8).jpg"
+        "img":"./img/gallery sushi (8).jpg",
+                "texto": "Lorem ipsum",
+        "precio": 10.00,
     }]
 
 
@@ -36,9 +52,17 @@ Como return ponemos la tarjeta que queremos que se nos muestre*/
     const mapGallery = photos.map(
         (photo)=>{
             return(`  
-                <div class="photo">
+            <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="photo--front">
                     <img src="${photo.img}" alt="Gallery">
-                </div>`)
+                </div>
+                <div class="photo--back">
+                    <p>${photo.texto}</p>
+                    <p>${photo.precio} €</p>
+                </div>
+            </div>
+        </div>`)
         }
         /* Se pone photo.img porque photo es el parámetro de la función map e img es como hemos nombrado en el array a cada elemento y es lo que queremos que se nos pinte en la tarjeta */
         )
@@ -46,17 +70,4 @@ Como return ponemos la tarjeta que queremos que se nos muestre*/
 
         /* Dentro de la caja que habíamos creado hacemos un innerHTML del map y le ponemos el método join para que separe cada elemento*/
     gallery.innerHTML=mapGallery.join("")
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
